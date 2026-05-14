29af0627ff85a6013c2efc8cf49d994bbaf821ef
Ria Caya <riacaya99@gmail.com>
2026-05-14 09:45:56 +0700

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
+const express = require("express");
+const play = require("play-dl");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Radio Senja hidup 🌙");
+});
+
+app.get("/play", async (req, res) => {
+  try {
+    const id = req.query.id;
+
+    const url = `https://www.youtube.com/watch?v=${id}`;
+
+    const stream = await play.stream(url);
+
+    res.setHeader("Content-Type", "audio/mpeg");
+
+    stream.stream.pipe(res);
+
+  } catch (err) {
+    res.send("Error: " + err.message);
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server hidup di port " + PORT);
+});
